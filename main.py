65d13a815979f94cb4c9a731747527bbf5b75664
OtakuMuch <nanimo1223@protonmail.com>
2024-10-27 15:57:44 +0800

    Added Rate Limits

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import discord
 import os
+import asyncio
 from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
 channel1id = int(os.getenv('ID1'))
 channel2id = int(os.getenv('ID2'))
 
@@ -21,7 +21,15 @@ async def on_message(message):
     if message.channel.id == channel1id:
         channeltosend = client.get_channel(channel2id)
         if channeltosend:
-            await channeltosend.send(message.content)
+            try:
+                await channeltosend.send(message.content, embed=message.embeds[0])
+            except discord.HTTPException as e:
+                if e.status == 429:
+                    retry_after = e.retry_after
+                    print(f"Rate limit hit. Retrying after {retry_after} seconds.")
+                    await asyncio.sleep(retry_after)
+                    await channeltosend.send(message.content, embed=message.embeds[0])
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
